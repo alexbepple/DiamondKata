289e5cc1edca5326de414b10e02afc1bc50a728e
Peter Fichtner (pfichtner) <github.2012.07.fichtner@spamgourmet.com>
2016-08-27 20:05:20 +0200

    resolved fake

--- src/DiamondTest.java
+++ src/DiamondTest.java
@@ -3,12 +3,13 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 import java.util.Arrays;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.List;
 
 import org.junit.Test;
 
 public class DiamondTest {
+
+	private static final String BACKGROUND_CHARACTER = "-";
 
 	@Test
 	public void createsDiamondForOneLetter() {
@@ -18,15 +19,31 @@ public class DiamondTest {
 	@Test
 	public void createsDiamondForTwoLetters() {
 		assertThat(diamond("AB"), is("-A-\n" + "B-B\n" + "-A-"));
+		assertThat(diamond("BA"), is("-B-\n" + "A-A\n" + outmost("B")));
 	}
 
 	private String diamond(String string) {
-		if (string.length() > 1) {
-			String linebreak = "\n";
-			return Arrays.asList("-A-", "B-B", "-A-").stream()
-					.collect(joining(linebreak));
+		String linebreak = "\n";
+		if (string.length() == 2) {
+			return join(linebreak, Arrays.asList(
+					outmost(string.substring(0, 1)),
+					centerline(string.substring(1, 2)),
+					outmost(string.substring(0, 1))));
 		}
+
 		return string;
+	}
+
+	private String centerline(String ch) {
+		return ch + BACKGROUND_CHARACTER + ch;
+	}
+
+	private String outmost(String ch) {
+		return BACKGROUND_CHARACTER + ch + BACKGROUND_CHARACTER;
+	}
+
+	private String join(String linebreak, List<String> lines) {
+		return lines.stream().collect(joining(linebreak));
 	}
 
 }
